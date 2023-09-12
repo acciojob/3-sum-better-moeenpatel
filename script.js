0fec9d6b9a5061dddbdcbd52c4dc3f5e9c76afab
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-12 21:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ function threeSum(arr, target) {
 	
 	arr.sort((a, b) => a - b);
   
-  // Initialize variables
   let closestSum = arr[0] + arr[1] + arr[2];
   
-  // Iterate through the array
   for (let i = 0; i < arr.length - 2; i++) {
     let left = i + 1;
     let right = arr.length - 1;
@@ -14,7 +12,6 @@ function threeSum(arr, target) {
     while (left < right) {
       const sum = arr[i] + arr[left] + arr[right];
       
-      // Check if the current sum is closer to the target
       if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
         closestSum = sum;
       }
@@ -24,7 +21,6 @@ function threeSum(arr, target) {
       } else if (sum > target) {
         right--;
       } else {
-        // Found an exact match, so return the sum
         return sum;
       }
     }
