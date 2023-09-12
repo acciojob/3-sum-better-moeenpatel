06148527039714baeee87d71272c7b7451b72d21
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-09-12 21:47:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  let n=arr.length;
+	let closestSum=1000000000;
+	for(let i=0;i<n-2;i++){
+		let j=i+1;
+		let k=n-1;
+		while(j<k){
+			let sum=arr[i]+arr[j]+arr[k];
+			if(Math.abs(target-closestsum)>(target-sum)){
+				 closestsum=sum;
+			}
+			if(sum>target){
+				k--;
+			}
+			else j++;
+		}
+	}
+	return closestsum;
 }
 
 module.exports = threeSum;
